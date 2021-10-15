4cf852a1379fdb15c261a7a4f0f26a183b5e6891
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-10-15 13:34:42 +0530

    Reformatted Previous Python Codes and Added My New Python Codes

--- Compound_interest.py
+++ Compound_interest.py
@@ -1,10 +1,12 @@
 def compound_interest(principle, rate, time):
- 
+
     # Calculates compound interest
     Amount = principle * (pow((1 + rate / 100), time))
     CI = Amount - principle
     print("Compound interest is", CI)
-a=input()
-b=input()
-c=input()
+
+
+a = input()
+b = input()
+c = input()
 compound_interest(a, b, c)
